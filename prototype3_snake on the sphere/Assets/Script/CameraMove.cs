dcc34abe4505064f25e94df266b456f0d41defae
mangAI <2385914340@qq.com>
2019-02-26 18:02:33 -0500

    beta1.2

--- prototype3_snake on the sphere/Assets/Script/CameraMove.cs
+++ prototype3_snake on the sphere/Assets/Script/CameraMove.cs
@@ -6,7 +6,7 @@ public class CameraMove : MonoBehaviour
 {
 
     const float rotateSpeed = 2f;
-    private  int rotateDegree = 0;
+   // private  int rotateDegree = 0;
     // Use this for initialization
     void Start()
     {
@@ -32,16 +32,16 @@ public class CameraMove : MonoBehaviour
         {
             this.transform.RotateAround(Vector3.zero, Vector3.up, rotateSpeed);
         }
-        else if (Input.GetKey(KeyCode.S) && rotateDegree > -30)
-        {
-            this.transform.RotateAround(Vector3.zero, -this.transform.right, rotateSpeed / 2);
-            rotateDegree--;
-        }
-        else if (Input.GetKey(KeyCode.W) && rotateDegree < 30)
-        {
-            this.transform.RotateAround(Vector3.zero, this.transform.right, rotateSpeed / 2);
-            rotateDegree++;
-        }
+        //else if (Input.GetKey(KeyCode.S) && rotateDegree > -30)
+        //{
+        //    this.transform.RotateAround(Vector3.zero, -this.transform.right, rotateSpeed / 2);
+        //    rotateDegree--;
+        //}
+        //else if (Input.GetKey(KeyCode.W) && rotateDegree < 30)
+        //{
+        //    this.transform.RotateAround(Vector3.zero, this.transform.right, rotateSpeed / 2);
+        //    rotateDegree++;
+        //}
     }
 
 }
